201030e6674d3da37d31e6d3d3a90aab37af4630
tamoshu <neo.tamoshu242@gmail.com>
2020-04-05 10:24:08 +0900

    postgres対応

--- hachione/config.py
+++ hachione/config.py
@@ -1,2 +1,8 @@
-SQLALCHEMY_DATABASE_URI = 'sqlite:///hachione.db'
+#SQLALCHEMY_DATABASE_URI = 'sqlite:///hachione.db'
+SQLALCHEMY_DATABASE_URI = 'postgresql://{user}:{password}@{host}/{name}'.format(**{
+    'user': 'usr',
+    'password': 'pass',
+    'host': '127.0.0.1',
+    'name': 'hachionemap'
+})
 SECRET_KEY = 'secret tamotamo'
